55916a0883e2a565d40914fa34409edc16f4a2e0
Paulo Becker <beckerpaulo81@gmail.com>
2026-09-13 15:50:12 -0300

    Configuração do PostgreSQL no TypeORM e ajustes de ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { typeOrmConfig } from './database/typeorm.config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), UsersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(typeOrmConfig),
+    UsersModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
